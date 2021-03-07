a0023ffffb29d1dac307df41d36f01760b9dcade
Damian <damian2927@gmail.com>
2021-03-07 11:23:24 +0100

    dodanie zdjec, poprawa zdjec, delikatna poprawa seo

--- js/carousel.js
+++ js/carousel.js
@@ -1,60 +1,8 @@
-$('.owl-content-2').owlCarousel({
-  center: true,
-  loop:true,
-  autoplay:false,
-  items:1,
-  autoHeight:true,
-});
-
-$('.owl-content-2-inside').owlCarousel({
-  center: true,
-  items:2,
-  loop:true,
-  margin:10,
-  dots: false,
-  autoplay:true,
-  autoplayTimeout:1000,
-  autoplayHoverPause:true,
-  responsive:{
-      600:{
-          items:4
-      }
-  }
-});
-
-$('.owl-content-4').owlCarousel({
-  center: true,
-  items:2,
-  loop:true,
-  margin:10,
-  autoplay:true,
-  autoplayTimeout:2000,
-  autoplayHoverPause:true,
-  autoHeight:true,
-  responsive:{
-      600:{
-          items:3
-      }
-  }
-});
-
+$(".owl-content-2").owlCarousel({center:!0,loop:!0,autoplay:!1,items:1,autoHeight:!0}),$(".owl-content-2-inside").owlCarousel({center:!0,items:2,loop:!0,margin:10,dots:!1,autoplay:!0,autoplayTimeout:1e3,autoplayHoverPause:!0,responsive:{600:{items:4}}}),$(".owl-content-4").owlCarousel({center:!0,items:2,loop:!0,margin:10,autoplay:!0,autoplayTimeout:2e3,autoplayHoverPause:!0,autoHeight:!0,responsive:{600:{items:3}}});
+var owl = $('.owl-carousel');
 owl.on('drag.owl.carousel', function(event) {
-  $('body').css('overflow', 'hidden');
+    $('body').css('overflow', 'hidden');
 });
-
 owl.on('dragged.owl.carousel', function(event) {
-  $('body').css('overflow', 'auto');
-});
-
-$('.owl-content-2').on('mousewheel', '.owl-stage', function (e) {
-if (e.deltaY>0) {
-  $('.owl-content-2').trigger('next.owl');
-} else {
-  $('.owl-content-2').trigger('prev.owl');
-}
-e.preventDefault();
-});
-
-$(document).ready(function(){
-$(".owl-carousel").owlCarousel();
+    $('body').css('overflow', 'auto');
 });
